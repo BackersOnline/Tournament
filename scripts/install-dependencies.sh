c7fdd8d32d3af1e58957561091492e9b5551c537
Nic Bonetto <nick.bonetto@gmail.com>
2018-01-30 13:52:00 -0800

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,4 +1,12 @@
-sudo yum update -y
+sudo yum update
+
+sudo yum install ruby
+sudo yum install wget
+
+cd /home/ec2-user
+wget https://aws-codedeploy-us-west-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
 
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum -y install nodejs
