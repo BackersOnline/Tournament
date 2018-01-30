8cc1aa3b7d3bbb3faa6ddc71be3c069a2f5a6a6b
Nic Bonetto <nick.bonetto@gmail.com>
2018-01-30 15:29:06 -0800

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,17 +1,1 @@
 sudo yum update
-
-sudo yum install -y httpd
-
-sudo yum install ruby
-sudo yum install wget
-
-cd /home/ec2-user
-wget https://aws-codedeploy-us-west-1.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-sudo yum -y install nodejs
-
-sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo yum install yarn
