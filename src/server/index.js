b3222127765a2fc3b50cf8f8c36c620aca49fd95
Nic Bonetto <nick.bonetto@gmail.com>
2018-02-01 09:20:46 -0800

    Created catch-all route for SPA functionality. #5

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 const app = express()
 
 app.use(express.static(__dirname + '/../client/static/'))
+app.use(express.static(__dirname + '/../client/static/build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../client/static/index.html'))
+})
 
 app.listen(process.env.PORT)
