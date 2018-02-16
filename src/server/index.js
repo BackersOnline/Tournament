c81e78e9dd46b4e98d68dd05b7719de87f96b496
Nic Bonetto <nick.bonetto@gmail.com>
2018-02-16 09:54:37 -0800

    Added middleware for body-parser. #16

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,18 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import bodyParser from 'body-parser';
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(express.static(__dirname + '/../client/static/'))
-app.use(express.static(__dirname + '/../client/static/build'))
-app.use(express.static(__dirname + '/../client/static/images'))
+app.use(express.static(__dirname + '/../client/static/'));
+app.use(express.static(__dirname + '/../client/static/build'));
+app.use(express.static(__dirname + '/../client/static/images'));
+app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../client/static/index.html'))
-})
+  res.sendFile(path.resolve(__dirname + '/../client/static/index.html'));
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
