bd3f91a7fcada04a20d575ef9bc8a9ac83d446ac
Nic Bonetto <nick.bonetto@gmail.com>
2018-02-28 15:41:07 -0800

    Added columns to users schema

--- src/database/users.sql
+++ src/database/users.sql
@@ -3,9 +3,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id              serial,
   username        text,
+  suffix          text,
   first_name      text,
   last_name       text,
+  middle_name     text,
   email           text,
   phone           text,
+  salutation      text,
   default_address text
 );
