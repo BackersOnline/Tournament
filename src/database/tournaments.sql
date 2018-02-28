a50065730dda4051c24f3f037de3fca1d65bccf2
Nic Bonetto <nick.bonetto@gmail.com>
2018-02-28 15:39:00 -0800

    Added column to tournaments schema

--- src/database/tournaments.sql
+++ src/database/tournaments.sql
@@ -4,6 +4,7 @@ CREATE TABLE tournament (
   id               serial,
   event_id         int,
   max_participants int,
+  min_participants int,
   buyin            int,
   guarantee        int,
   terms            text
